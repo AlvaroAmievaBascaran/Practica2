b2bf20645fe2548e4a856be2c76aae99d646dc49
Alvaro <alvaroamievabascaran@gmail.com>
2026-03-05 12:49:47 +0100

    Detalle

--- src/main/java/edu/comillas/icai/gitt/pat/spring/Practica3/Practica3Application.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/Practica3/Practica3Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Practica3Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Practica2Application.class, args);
+		SpringApplication.run(Practica3Application.class, args);
 	}
 
 }
